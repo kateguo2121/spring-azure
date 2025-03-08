bf80616af520072a7ec2930c0c48ac6bee4ee5f9
Kate Guo <guok@appleton.millsfleetfarm.com>
2025-03-07 20:42:29 -0600

    first commit

---
+++ src/main/java/com/example/azure/controller/GreetController.java
@@ -0,0 +1,12 @@
+package com.example.azure.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetController {
+    @GetMapping("/greet")
+    public String greet() {
+        return "Hello from Azure!";
+    }
+}
